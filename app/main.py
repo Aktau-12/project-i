e61d578664eee97e1d368c841ab7d62f9b9fcbf5
Aktau-12 <axetime@mail.ru>
2025-04-17 13:51:16 +0500

    Исправил импорт: добавил app.routes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-from routes import habit  # ✅ добавлено
-
+from app.routes import habit  # ✅ добавлено
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
