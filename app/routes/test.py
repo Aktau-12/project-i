417be700c21b838c9af1ac046564353f58fc83a1
Aktau-12 <axetime@mail.ru>
2025-04-21 19:34:26 +0500

    fix: remove UserAnswer import from routes/test.py

--- app/routes/test.py
+++ app/routes/test.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database.db import SessionLocal
-from app.models.test import Test, Question, UserAnswer, UserResult
+from app.models.test import Test, Question, UserResult
 from app.models.user import User
 from app.routes.auth import get_current_user
 from pydantic import BaseModel
